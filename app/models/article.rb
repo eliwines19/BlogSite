e0c9617cd701b23a583ceeb74a03ea81f428ee85
eliwines19 <wines.elijah@gmails.com>
2020-07-30 16:21:54 -0400

    made text validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 
-    validates :title, presence: true, length: { minimum: 5 }
+    validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
+    validates :text, presence: true
 
 end
